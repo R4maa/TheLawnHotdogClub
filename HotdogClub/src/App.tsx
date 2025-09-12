8996e13dbcdae017df83db3b4893a4668fd8158a
R4maa <amarrazzaq1@gmail.com>
2025-09-12 00:47:23 -0400

    .

--- HotdogClub/src/App.tsx
+++ HotdogClub/src/App.tsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <>
       <div className='background'>
+        <img className='logo' src='TheLawnLogo.png'></img>
         <h1>Welcome to The Lawn Hotdog Club</h1>
+        <p>More to come soon!</p>
       </div>
     </>
   )
