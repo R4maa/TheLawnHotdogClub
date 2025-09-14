efd04b434ac4bed469ce7880011fe0b0e4b29fa7
Amar Razzaq <amarrazzaq1@gmail.com>
2025-09-14 01:41:08 -0400

    feed Init

--- HotdogClub/src/App.tsx
+++ HotdogClub/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
+import Feed from './Feed.tsx'
 
 function App() {
   return (
     <>
       <div className='background'>
         <img className='logo' src='TheLawnLogo.png'></img>
-        <h1>Welcome to The Lawn Hotdog Club</h1>
+        <h1>Welcome to The Lawn Hotdog Club!</h1>
         <p>More to come soon!</p>
+        <Feed />
       </div>
     </>
   )
---
+++ HotdogClub/src/Feed.tsx
@@ -0,0 +1,13 @@
+import './Feed.css'
+
+function Feed() {
+  return (
+  <>
+    <div>
+        <p>Hello World! This is a test. Awesome!</p>
+    </div>
+  </>
+  )
+}
+
+export default Feed
